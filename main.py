12fedf7ade326cd2ee4f7480c5737efad4608032
vijay <elderpatha@gmail.com>
2021-04-03 20:02:12 -0700

    button and input

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("My first GUI")
 window.minsize(width=500, height=500)
 
-#Label
-label = tkinter.Label(text="Im a label", font=("Arial", 24, "bold"))
-label.pack() # puts the text in the screen
+# Label
+label = Label(text="Im a label", font=("Arial", 24, "bold"))
+label.pack()  # puts the text in the screen
+
+label["text"] = "New Text"
+label.config(text="New Text")
 
 
+# Button
+def button_clicked():
+    print("I got clicked")
+    # label.config(text="button got clicked")
+    label.config(text=input.get())
 
-window.mainloop() # keeps the screen running
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
+input.get()
+
+window.mainloop()  # keeps the screen running
--- venv/notes.md
+++ venv/notes.md
@@ -1,7 +1,7 @@
 #Advance Python Arguements
 
 ## Key word Arguments
-```
+```buildoutcfg
 def my_function(a, b, c):
     #Do this with a
     #Then do this with b
@@ -9,7 +9,7 @@ def my_function(a, b, c):
 my_function(c=3, a = 1, b=2)
 ```
 ## Defualt value Arugments 
-```
+```buildoutcfg
 def my_function(a=1, b=2, c=3):
     #Do this with a
     #Then do this with b
@@ -17,8 +17,23 @@ def my_function(a=1, b=2, c=3):
 my_function()
 0r my_function(b=5) # if I want to chage the value of the b
 ```
+## Unlimited Positional Arguments 
 
+```buildoutcfg
+def add(*args):
+    sum = 0
+    for n in args:
+        return sum += n
+print(add(1,3,5,789))
 
+```
+## Unlimited keyword Arguments 
+```buildoutcfg
+def calculate(**kwargs):
+    print(kwargs) # kwargs is dict with {add:3, multiply:5) 
+    
+print(calculate(add=3, multiply=5)
+```
 
 
 
