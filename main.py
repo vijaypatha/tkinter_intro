6c12b9173a3c76861ba0368fb7a553a31a533888
vijay <elderpatha@gmail.com>
2021-04-03 20:20:58 -0700

    button and input

--- main.py
+++ main.py
@@ -4,28 +4,37 @@ window = Tk()
 
 window.title("My first GUI")
 window.minsize(width=500, height=500)
-
-# Label
-label = Label(text="Im a label", font=("Arial", 24, "bold"))
-label.pack()  # puts the text in the screen
-
-label["text"] = "New Text"
-label.config(text="New Text")
+window.config(padx=20,pady=20)
 
 
-# Button
 def button_clicked():
     print("I got clicked")
     # label.config(text="button got clicked")
     label.config(text=input.get())
 
 
-button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# TODO: Label Widget
+label = Label(text="Im a label", font=("Arial", 24, "bold"))
+# label.pack()  # puts the text in the screen
+# label.place(x=100 , y=200) Precise coordinate
+label.grid(column=0, row=0)
 
-# Entry
+label["text"] = "New Text"
+label.config(text="New Text")
+
+# TODO: Button Widget
+button = Button(text="Click Me", command=button_clicked)
+# button.pack()
+button.grid(column=1, row=1)
+
+# TODO: New Button Widget
+new_button = Button(text="double click me")
+new_button.grid(column=2, row=0)
+
+# TODO: Entry Widget
 input = Entry(width=10)
-input.pack()
+# input.pack()
+input.grid(column=3, row=2)
 input.get()
 
 window.mainloop()  # keeps the screen running
